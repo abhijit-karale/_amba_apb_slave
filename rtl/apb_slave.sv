a17b585fb68f21532db4b5858df2632a520c8296
Abhijit Karale <abhijitkarale8@gmail.com>
2026-09-04 06:30:55 +0530

    Fix comment formatting in apb_slave.sv

--- rtl/apb_slave.sv
+++ rtl/apb_slave.sv
@@ -1,7 +1,7 @@
  // -----------------------------------------------------------------------------
  // Module      : apb_slave
  // Description : AMBA APB (v2.0) slave peripheral with 4 memory-mapped
-//               32-bit registers (REG0-REG3). Implements SETUP/ACCESS
+ //               32-bit registers (REG0-REG3). Implements SETUP/ACCESS
 //               APB state machine per AMBA APB protocol specification.
 // Author      : Abhijit Karale
 // -----------------------------------------------------------------------------
